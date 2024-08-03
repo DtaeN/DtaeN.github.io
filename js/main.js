ee4b497d5be992872264bbf3438fe69a349d40c2
DtaeN <4390269@gamil.com>
2024-08-03 21:51:16 +0300

    1

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let nam = 0;
-const el = document.getElementById('nam')
-
-function test () {
-	nam ++
-	console.log(nam);
-	el.innerHTML = nam
-}
---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+let tg = window.Telegram.WabApp;
+
+let dishes = {};
+
+tg.expand();
+
+
+function buy (id) {
+	document.getElementById(`buy_${id}`).style.display = 'none';
+	document.getElementById(`sup_${id}`).style.display = 'flex';
+
+	dishes[id] = 1;
+	console.log(dishes);
+	document.getElementById(`counter_${id}`).innerHTML = dishes[id];
+	tg.MainButton.show()
+}
+
+
+function plus (id) {
+	dishes[id] += 1;
+	console.log(dishes);
+	document.getElementById(`counter_${id}`).innerHTML = dishes[id];
+}
+
+
+function minutes (id) {
+	dishes[id] -= 1;
+
+	if (dishes[id] <= 0){
+		document.getElementById(`buy_${id}`).style.display = 'inline-block';
+		document.getElementById(`sup_${id}`).style.display = 'none';
+	} else {
+		document.getElementById(`counter_${id}`).innerHTML = dishes[id];
+	}
+	console.log(dishes);
+}
+
+
+Telegram.WabApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(JSON.stringify(dishes));
+})
