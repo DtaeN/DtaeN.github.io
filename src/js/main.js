cdb19dfd7cf8ece9e8b7a69cee1822a0a30dcc40
DtaeN <4390269@gamil.com>
2024-08-08 17:56:44 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -6,28 +6,6 @@ Telegram.WebApp.expand();
 Telegram.WebApp.MainButton.setText("Заказать");
 Telegram.WebApp.MainButton.color = "#12f00d";
 
-function sendDataToTelegram(data) {
-  // Формируем URL с данными
-  const url = new URL('https://t.me/experiment666_bot');
-  url.searchParams.set('start', `send_data_${data}`);
-
-  // Создаем HTTP-запрос
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url.toString(), true);
-
-  // Обрабатываем ответ
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log('Данные успешно отправлены в Telegram бот');
-    } else {
-      console.error('Ошибка при отправке данных в Telegram бот:', xhr.status);
-    }
-  };
-
-  // Отправляем запрос
-  xhr.send();
-}
-
 function buy (id) {
 	document.getElementById(`buy_${id}`).style.display = 'none';
 	document.getElementById(`sup_${id}`).style.display = 'flex';
@@ -61,7 +39,6 @@ function minutes (id) {
 
 Telegram.WebApp.onEvent('mainButtonClicked', function() {
 	console.log("!!!");
-	sendDataToTelegram("!!!");
-	// Telegram.WebApp.sendData(JSON.stringify(dishes));
-	// Telegram.WebApp.close();
+	Telegram.WebApp.sendData(JSON.stringify(dishes));
+	Telegram.WebApp.close();
 });
