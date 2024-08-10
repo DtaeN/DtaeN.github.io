ddb2a5c42ca09638ff267603fdf03180e9ed843a
DtaeN <4390269@gamil.com>
2024-08-10 23:41:09 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -30,7 +30,7 @@ function minutes (id) {
 	if (dishes[id] <= 0){
 		document.getElementById(`buy_${id}`).style.display = 'inline-block';
 		document.getElementById(`sup_${id}`).style.display = 'none';
-		delete dishes(id)
+		delete dishes[id]
 	} else {
 		document.getElementById(`counter_${id}`).innerHTML = dishes[id];
 	}
